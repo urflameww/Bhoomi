e70667b0f37bf2222f8b14dfb9fdf529b220a957
urflameww <madakadinesh2011@gmail.com>
2026-04-05 11:15:31 +0530

    Update index.ps1

--- index.ps1
+++ index.ps1
@@ -1,6 +1,3 @@
-# Force UTF-8 Encoding to fix the ruined ASCII art
-[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
-
 # BHOOMI REMOTE INTERFACE
 Clear-Host
 $C = [ConsoleColor]::Cyan
@@ -9,14 +6,14 @@ Write-Host "--- BHOOMI CISCO RECOVERY INTERFACE ---" -ForegroundColor $C
 $pass = Read-Host "Enter Access Password"
 
 if ($pass -eq "Dinesh") {
-    Write-Host "[+] Authentication Successful. Fetching Engine..." -ForegroundColor Green
-    $url = "https://gist.github.com/urflameww/e0685a4f3ab6325dceda596f7f45d611"
+    Write-Host "[+] Authentication Successful. Launching Engine..." -ForegroundColor Green
+    
+    # URL to your Python Gist
+    $url = "https://gist.githubusercontent.com/urflameww/e0685a4f3ab6325dceda596f7f45d611/raw/Bhoomi.py"
     $code = (New-Object Net.WebClient).DownloadString($url)
     
-    # Execute Python in RAM
-    $encoded = [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($code))
-    # We also pass the UTF-8 flag to Python
-    python -X utf8 -c "import base64; exec(base64.b64decode('$encoded').decode('utf-8'))"
+    # This pipes the code into Python so we don't hit the character limit
+    $code | python -
 } else {
     Write-Host "[-] Access Denied." -ForegroundColor Red
     Start-Sleep -Seconds 2
